e6a835f6e167806a701f57fe7a12fecd7b5462fc
German Enriquez <enriquez.german.e@gmail.com>
2014-09-21 21:15:14 -0300

    Fixed retro-IE styles issues

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ cp -r code/static/** publish/
 
 tools/lessjs/bin/lessc -x publish/css/print.less > publish/css/print.css
 tools/lessjs/bin/lessc -x publish/css/bundle.less > publish/css/bundle.css
-sed -E "s/([\,\}\{ ])(header|nav|section|article|footer|aside)/\1.html-\2/g" publish/css/bundle.css >  publish/css/bundle.ie5.css
+sed -E "s/([\,\}\{ ])(header|nav|section|article|footer|aside)/\1.html-\2 /g" publish/css/bundle.css >  publish/css/bundle.ie5.css
 
 cd code/templates
 ../../tools/compiletemplate.py 'index' '../../publish/'
